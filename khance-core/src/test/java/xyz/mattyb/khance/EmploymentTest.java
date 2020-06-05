7b82296c603380b3261ac370a15955c1cadcf569
Matt Berteaux <mattyb678@gmail.com>
2020-06-04 22:56:54 -0600

    Test companies

---
+++ khance-core/src/test/java/xyz/mattyb/khance/EmploymentTest.java
@@ -0,0 +1,16 @@
+package xyz.mattyb.khance;
+
+import org.junit.jupiter.api.Test;
+import xyz.mattyb.khance.testutils.BaseChanceTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static xyz.mattyb.khance.test.utils.TestUtils.thousand;
+
+public class EmploymentTest extends BaseChanceTest {
+
+    @Test
+    public void testCompanies() {
+        thousand(i -> assertThat(CompaniesKt.getCompanies(), hasItem(chance.employment.company())));
+    }
+}
