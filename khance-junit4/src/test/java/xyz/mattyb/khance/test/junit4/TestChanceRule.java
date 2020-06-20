08721c71c37ee18c528a598ab2b3cf4089218c76
Matt Berteaux <mattyb678@gmail.com>
2020-06-19 22:55:26 -0600

    Seeded chance rule test

--- khance-junit4/src/test/java/xyz/mattyb/khance/test/junit4/TestChanceRule.java
+++ khance-junit4/src/test/java/xyz/mattyb/khance/test/junit4/TestChanceRule.java
@@ -14,6 +14,9 @@ public class TestChanceRule {
     @Rule
     public ChanceRule chanceRule = new ChanceRule();
 
+    @Rule
+    public ChanceRule seeded = new ChanceRule(1986L);
+
     @Test
     public void testChance() {
         final AtomicInteger trueCount = new AtomicInteger(0);
@@ -35,5 +38,18 @@ public class TestChanceRule {
             assertThat(populated.getAge(), is(allOf(greaterThan(-1), lessThan(101))));
             assertThat(populated.getYo().length(), is(15));
         });
+    }
+
+    @Test
+    public void testSeeded() {
+        final AtomicInteger trueCount = new AtomicInteger(0);
+
+        thousand(i -> {
+            if (seeded.getChance().bool()) {
+                trueCount.incrementAndGet();
+            }
+        });
+
+        assertThat(trueCount.get(), is(allOf(greaterThan(200), lessThan(800))));
     }
 }
